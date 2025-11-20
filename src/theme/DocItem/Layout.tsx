613e3615cbfac9a8452a609a73d85a005f3abd28
advik555 <141858080+Advik555wastaken@users.noreply.github.com>
2025-11-20 13:42:40 -0500

    Update Layout.tsx

--- src/theme/DocItem/Layout.tsx
+++ src/theme/DocItem/Layout.tsx
@@ -3,11 +3,9 @@ import OriginalDocItemLayout from '@theme-original/DocItem/Layout';
 import EditPageTop from '@site/src/components/EditPageTop';
 
 export default function Layout(props) {
-  const editUrl = props.content?.metadata?.editUrl;
-
   return (
     <>
-      <EditPageTop editUrl={editUrl} />
+      <EditPageTop />
       <OriginalDocItemLayout {...props} />
     </>
   );
