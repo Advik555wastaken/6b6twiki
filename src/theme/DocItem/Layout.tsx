6cc58366a52c2b8db03820ba9cb8890fca25372c
advik555 <141858080+Advik555wastaken@users.noreply.github.com>
2025-11-20 13:49:03 -0500

    Add editUrl to EditPageTop in Layout component
    Pass editUrl to EditPageTop component for improved editing experience.

--- src/theme/DocItem/Layout.tsx
+++ src/theme/DocItem/Layout.tsx
@@ -3,10 +3,13 @@ import OriginalDocItemLayout from '@theme-original/DocItem/Layout';
 import EditPageTop from '@site/src/components/EditPageTop';
 
 export default function Layout(props) {
+  // Docusaurus already provides the fully correct editUrl per doc
+  const editUrl = props.content?.metadata?.editUrl;
+
   return (
     <>
-      <EditPageTop />
-      <OriginalDocItemLayout {...props} />
+      <EditPageTop editUrl={editUrl} />  {/* Top button */}
+      <OriginalDocItemLayout {...props} /> {/* Original layout, including bottom edit link */}
     </>
   );
 }
