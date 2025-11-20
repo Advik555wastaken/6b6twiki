ce1b64a66117d5ac5f9f20db6d27249ff50e5987
advik555 <141858080+Advik555wastaken@users.noreply.github.com>
2025-11-20 13:39:30 -0500

    Pass editUrl to EditPageTop component

--- src/theme/DocItem/Layout.tsx
+++ src/theme/DocItem/Layout.tsx
@@ -3,9 +3,11 @@ import OriginalDocItemLayout from '@theme-original/DocItem/Layout';
 import EditPageTop from '@site/src/components/EditPageTop';
 
 export default function Layout(props) {
+  const editUrl = props.content?.metadata?.editUrl;
+
   return (
     <>
-      <EditPageTop />
+      <EditPageTop editUrl={editUrl} />
       <OriginalDocItemLayout {...props} />
     </>
   );
